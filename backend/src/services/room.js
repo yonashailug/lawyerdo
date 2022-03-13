cfd696761314420f7a3b928a488ff2a4113f7f2d
Yonas Hailu <yonashailug@gmail.com>
2022-03-13 14:12:24 -0500

    update button component

--- backend/src/services/room.js
+++ backend/src/services/room.js
@@ -1,8 +1,46 @@
 const roomModel = require('../models/room')
+const userService = require('../services/user')
 
 module.exports = {
     allRooms: () => roomModel.find(),
-    addRoom: (data) => roomModel.create(data),
+    createRoom: async({ userId }) => {
+
+        const { name } = await userService.getById(userId)
+
+        try {
+
+            const requestData = {
+                name,
+                user: { 
+                    id: userId,
+                    name
+                },
+            }
+
+            const { data } = await makeRequest('rooms', requestData)
+
+            const room = {
+                roomId: data.room.id,
+                name,
+                userId: userId,
+                members: [userId],
+            }
+
+            return await this.roomsService.create(room)
+
+        } catch(error) {
+
+            if (!error.response)
+                throw new InternalServerErrorException({
+                    errors: 'Something went wrong, try again later.',
+                })
+
+            throw new HttpException({
+                errors: error.response.data.error
+            }, error.response.status)
+        }
+
+    },
     getRoomById: (id) => roomModel.findById(id),
     deleteRoomById: (id) => roomModel.deleteOne({ _id: id })
 }
