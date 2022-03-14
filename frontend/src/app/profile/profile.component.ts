3d0bf363f8dad12728893799fd5b990571669f1b
Yonas Hailu <yonashailug@gmail.com>
2022-03-14 17:46:07 -0500

    update header display

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { User } from '../shared/model/user';
 import { TokenService } from '../shared/services/token.service';
+import { store } from '../shared/store/store';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styles: [],
 })
-export class ProfileComponent implements OnInit {
-  user: any;
-  constructor(private tokenService: TokenService) {}
+export class ProfileComponent implements OnInit, OnDestroy {
+  //user2: any;
+  @Input() user: User = User.EMPTY_USER
+  subscription: any = {};
+
+  constructor(private tokenService: TokenService) {
+    this.subscription = store.subscribe(() => {
+      this.user = store.getState().user
+    })
+  }
 
   ngOnInit(): void {
-    this.user = this.tokenService.getUser();
+    // this.user = this.tokenService.getUser();
+    console.log(this.user.getName())
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
