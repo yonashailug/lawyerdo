8369a465bd07a7069df6a37acd177172ee581195
Yonas Hailu <yonashailug@gmail.com>
2022-03-15 13:51:12 -0500

    add modal

---
+++ frontend/src/app/shared/components/modals.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modals',
+  template: `
+    <p>
+      modals works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ModalsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
