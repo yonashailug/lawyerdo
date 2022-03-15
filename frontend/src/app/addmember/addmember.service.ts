c2dcc0c1a4b6d47e1382a5ae41c7e11919bdf3d4
Ochirtulga <ochiroocho@gmail.com>
2022-03-15 01:51:46 -0500

    add member component created

---
+++ frontend/src/app/addmember/addmember.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { BASE_URL } from '../config';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AddmemberService {
+  constructor(private httpClient: HttpClient) {}
+
+  checkMember(email: string, room: any[]): boolean {
+    let isMember = false;
+    for (let i = 0; i < room.length; i++) {
+      if (room[i].email === email) {
+        isMember = true;
+      }
+    }
+    if (isMember === true) return false;
+    return true;
+  }
+
+  addMember(email: string, roomId: string) {
+    return this.httpClient
+      .post(`${BASE_URL}/rooms/${roomId}/join`, email)
+      .pipe(
+        map(({ data }: any) =>
+          data.update === true || data.joined ? data : null
+        )
+      );
+  }
+
+  checkEmail(email: string): Observable<{ [key: string]: boolean } | null> {
+    return this.httpClient
+      .get(`${BASE_URL}/users/verify?email=${email}`)
+      .pipe(map(({ data }: any) => (data.exists ? data : null)));
+  }
+}
